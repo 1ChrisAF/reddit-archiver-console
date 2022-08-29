9ed00dd58d0d82cdd813f6cbee6720fd6ad84bb7
Christopher Flippo <christopher.allen.flippo@gmail.com>
2022-08-29 10:41:55 -0500

    Can pull the link on the next button w/o issue

--- Program.cs
+++ Program.cs
@@ -1,23 +1,58 @@
 ﻿using System.Text.RegularExpressions;
 
-string? username;
-Console.Write("Enter a username: ");
-username = Console.ReadLine();
-if (username != null) {
+main();
+
+void main() {
+    string username = GetUsername();
+    while (username == "") {
+        username = GetUsername();
+    }
+    Task<String> htmlTask = getProfileHTML(username);
+    string pageContents = htmlTask.Result;
+    MatchCollection linkCandidates = getLinksFromPage(pageContents);
+    string nextButtonLink = "";
+    if (linkCandidates.Count > 1) {
+        nextButtonLink = getNextButtonLink(linkCandidates);
+    }
+    Console.WriteLine(nextButtonLink);
+}
+
+string GetUsername() {
+    string username;
+    // Retrieve username from user
+    Console.Write("Enter a username: ");
+    username = Console.ReadLine();
+    if (username != "") {
+        return username;
+    } else {
+        Console.WriteLine("No username entered! Trying again...");
+        return "";
+    }
+}
+
+async Task<String> getProfileHTML(string username) {
     HttpClient client = new HttpClient();
     string url = "https://old.reddit.com/u/" + username;
+    Console.WriteLine("Checking {0}...", url);
     var response = await client.GetAsync(url);
     var pageContents = await response.Content.ReadAsStringAsync();
-    // Console.WriteLine(pageContents);
-    string nextButtonPattern = "a href=\"https://old\\.reddit\\.com/user/OrthodoxMemes\\?.*\" rel=\"nofollow next\"";
-    Regex regex = new Regex(nextButtonPattern);
-    var nextButtonPartial = regex.Matches(pageContents);
-    //string beginString = "https://old\\.reddit\\.com/user/OrthodoxMemes\\?";
-    nextButtonPattern = @"\b(?:https?://|www.)\S+\b";
-    //nextButtonPattern = beginString + nextButtonPattern;
-    regex = new Regex(nextButtonPattern);
-    nextButtonPartial = regex.Matches(nextButtonPartial[0].Value);
-    Console.Write(nextButtonPartial[0].Value);
-} else {
-    Console.WriteLine("No value entered!");
+    return pageContents;
+}
+
+MatchCollection getLinksFromPage(string pageContents) {
+    // Regex to parse page for all links
+    Regex regex = new Regex(@"\b(?:https?://|www.)\S+\b");
+    var linkCandidates = regex.Matches(pageContents);
+    return linkCandidates;
+}
+
+string getNextButtonLink(MatchCollection linkCandidates) {
+    Regex regex = new Regex("OrthodoxMemes\\?count");
+    string nextButton = "";
+    foreach (var link in linkCandidates) {
+        if (regex.IsMatch(link.ToString())) {
+            nextButton = link.ToString();
+        }
+    }
+    return nextButton;
 }
